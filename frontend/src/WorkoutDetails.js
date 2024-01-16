747523617dd80f5da07d7de28efd9f7552b11c2f
rishabh2727 <rishabhmehra2710@gmail.com>
2024-01-16 15:08:24 -0700

    Create WorkoutDetails.js

---
+++ frontend/src/WorkoutDetails.js
@@ -0,0 +1,41 @@
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+
+// date fns
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
+const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext()
+
+  const handleClick = async () => {
+    const response = await fetch('/api/workouts/' + workout._id, {
+      method: 'DELETE'
+    })
+    const json = await response.json()
+
+    if (response.ok) {
+      dispatch({type: 'DELETE_WORKOUT', payload: json})
+    }
+  }
+
+  return (
+    <div className="workout-details">
+      <h4>{workout.title}</h4>
+      <p><strong>Load (kg): </strong>{workout.load}</p>
+      <p><strong>Number of reps: </strong>{workout.reps}</p>
+      <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
+      <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
+    </div>
+  )
+}
+
+export default WorkoutDetails
+
+
+// Rendering each workout means displaying the details of
+// each workout on the webpage. In the context of React, rendering
+// refers to the process of turning your data into something that 
+// the user can see and interact with on the screen.
+
+// When we say "rendering each workout," it means taking the information 
+// (data) about a specific workout and using the WorkoutDetails component
+//  to visually represent that information on the webpage.
